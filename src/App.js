a5b7383212373059a788c74347758b3a0d4a6164
omaralhoori <omaralhoori@users.noreply.github.com>
2020-09-02 07:18:45 +0300

    add shop page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/home/homepage'
+import ShopePage from './pages/shop/shoppage'
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route  path='/shop' component={ShopePage} />
       </Switch>
     </div>
   );
