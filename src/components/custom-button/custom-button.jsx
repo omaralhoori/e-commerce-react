bd85227b53bc484fa57b2daef5d07828e1679327
omaralhoori <omaralhoori@users.noreply.github.com>
2020-09-02 19:27:30 +0300

    style the button for google sigin

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import './custom-button.scss'
- 
-const CustomButton = ({children, ...otherProps}) => {
+
+const CustomButton = ({ children, isGoogleSiginIn, ...otherProps }) => {
     return (
-        <button className="custom-button" {...otherProps}>
+        <button className={`${isGoogleSiginIn ? 'google-sigin-in' : ''} custom-button `} {...otherProps}>
             {children}
         </button>
     );
 }
- 
- 
+
+
 export default CustomButton;
