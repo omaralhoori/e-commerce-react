bc2690861f39a74b20dafce0d50b060121f20ec0
omaralhoori <omaralhoori@users.noreply.github.com>
2020-09-02 17:58:04 +0300

    create login register page

---
+++ src/pages/login-register/login-registerpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './login-registerpage.scss'
+
+import SignIn from '../../components/sigin-in/sigin-in'
+
+const LoginRegisterPage = () => {
+    return (
+        <div className='login-registerpage'>
+            <SignIn />
+        </div>
+    );
+}
+
+ 
+export default LoginRegisterPage;
