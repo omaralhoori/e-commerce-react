fc8dfa736b1de6bac3c266f32a725376fd94dde8
omaralhoori <omaralhoori@users.noreply.github.com>
2020-09-02 07:52:07 +0300

    add header to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/home/homepage'
 import ShopePage from './pages/shop/shoppage'
+import Header from './components/header/header'
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopePage} />
